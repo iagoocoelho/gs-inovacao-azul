dd5665698616871a2a4604a528f00834dd535a5a
iagocoelho-deal <iago.coelho@deal.com.br>
2024-06-04 14:57:00 -0300

    first

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world! <Button variant="destructive">Click me</Button>
+    </h1>
+  );
+}
+
+export default App;
